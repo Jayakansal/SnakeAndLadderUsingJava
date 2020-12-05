35a832fd69ce7b769722ff12c2010f6d4f553470
Jayakansal <21jayajindal@gmail.com>
2020-12-05 12:09:33 +0530

    Added:Rolling The Dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,25 @@
-public class SnakeAndLadder {
-	static final int PLAYER =1;
-	static final int STARTPOST=0;
+package com.java.SnakeAndLadder;
+import java.util.Random;
 
-	public static void main (String[] args)
-	{
-	}
-	}
+public class SnakeAndLadder {
+
+	//UC1 Variable Initialized at starting Positions
+	//public static int getIntilizePosition() {
+	     static final int PLAYER=1;
+	     static final int STARTPOSTION=0;
+	     
+      public static void main(String[] args)
+	 	{
+	 		System.out.println("Welcome to the Snake And Ladder Game");
+	 	}	
+
+    //UC2 Rolls the die 
+    static int Roll_Dice() {	
+	Random random=new Random();
+    int randomNum=random.nextInt(5)+1;
+    return randomNum;
+    }
+    
+	
+}
+
